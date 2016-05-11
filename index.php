dd34efa2c8e16de5e2f13ac20ef6c64ea4274e9c
yujin <jangy100@nate.com>
2016-05-11 16:34:59 +0900

    테스트입니다.
    커밋테스트

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENTfdsafsdfsd
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
