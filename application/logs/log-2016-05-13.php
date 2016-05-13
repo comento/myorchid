c751240fda7f73c6aa192ade4b0aa3395d2e1b47
Seobs <subi88@naver.com>
2016-05-13 18:01:36 +0900

    ready

--- application/logs/log-2016-05-13.php
+++ application/logs/log-2016-05-13.php
@@ -15,3 +15,106 @@ INFO - 2016-05-13 01:12:50 --> Language Class Initialized
 INFO - 2016-05-13 01:12:51 --> Loader Class Initialized
 INFO - 2016-05-13 01:12:51 --> Controller Class Initialized
 INFO - 2016-05-13 01:12:51 --> File loaded: /Applications/mampstack-5.6.21-2/apache2/htdocs/myorchid/application/views/templates/header.php
+INFO - 2016-05-13 01:29:25 --> Config Class Initialized
+INFO - 2016-05-13 01:29:25 --> Hooks Class Initialized
+DEBUG - 2016-05-13 01:29:25 --> UTF-8 Support Enabled
+INFO - 2016-05-13 01:29:25 --> Utf8 Class Initialized
+INFO - 2016-05-13 01:29:25 --> URI Class Initialized
+DEBUG - 2016-05-13 01:29:25 --> No URI present. Default controller set.
+INFO - 2016-05-13 01:29:25 --> Router Class Initialized
+INFO - 2016-05-13 01:29:25 --> Output Class Initialized
+INFO - 2016-05-13 01:29:25 --> Security Class Initialized
+DEBUG - 2016-05-13 01:29:25 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-05-13 01:29:25 --> Input Class Initialized
+INFO - 2016-05-13 01:29:25 --> Language Class Initialized
+INFO - 2016-05-13 01:29:25 --> Loader Class Initialized
+INFO - 2016-05-13 01:29:25 --> Controller Class Initialized
+INFO - 2016-05-13 01:29:25 --> File loaded: /Applications/mampstack-5.6.21-2/apache2/htdocs/myorchid/application/views/welcome_message.php
+INFO - 2016-05-13 01:29:25 --> Final output sent to browser
+DEBUG - 2016-05-13 01:29:25 --> Total execution time: 0.0430
+INFO - 2016-05-13 01:30:20 --> Config Class Initialized
+INFO - 2016-05-13 01:30:20 --> Hooks Class Initialized
+DEBUG - 2016-05-13 01:30:20 --> UTF-8 Support Enabled
+INFO - 2016-05-13 01:30:20 --> Utf8 Class Initialized
+INFO - 2016-05-13 01:30:20 --> URI Class Initialized
+DEBUG - 2016-05-13 01:30:20 --> No URI present. Default controller set.
+INFO - 2016-05-13 01:30:20 --> Router Class Initialized
+INFO - 2016-05-13 01:30:20 --> Output Class Initialized
+INFO - 2016-05-13 01:30:20 --> Security Class Initialized
+DEBUG - 2016-05-13 01:30:20 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-05-13 01:30:20 --> Input Class Initialized
+INFO - 2016-05-13 01:30:20 --> Language Class Initialized
+INFO - 2016-05-13 01:30:20 --> Loader Class Initialized
+INFO - 2016-05-13 01:30:20 --> Controller Class Initialized
+INFO - 2016-05-13 01:30:20 --> File loaded: /Applications/mampstack-5.6.21-2/apache2/htdocs/myorchid/application/views/welcome_message.php
+INFO - 2016-05-13 01:30:20 --> Final output sent to browser
+DEBUG - 2016-05-13 01:30:20 --> Total execution time: 0.0416
+INFO - 2016-05-13 01:57:40 --> Config Class Initialized
+INFO - 2016-05-13 01:57:40 --> Hooks Class Initialized
+DEBUG - 2016-05-13 01:57:40 --> UTF-8 Support Enabled
+INFO - 2016-05-13 01:57:40 --> Utf8 Class Initialized
+INFO - 2016-05-13 01:57:40 --> URI Class Initialized
+INFO - 2016-05-13 01:57:40 --> Router Class Initialized
+INFO - 2016-05-13 01:57:40 --> Output Class Initialized
+INFO - 2016-05-13 01:57:40 --> Security Class Initialized
+DEBUG - 2016-05-13 01:57:40 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-05-13 01:57:40 --> Input Class Initialized
+INFO - 2016-05-13 01:57:40 --> Language Class Initialized
+INFO - 2016-05-13 01:57:40 --> Loader Class Initialized
+INFO - 2016-05-13 01:57:40 --> Controller Class Initialized
+INFO - 2016-05-13 01:57:40 --> File loaded: /Applications/mampstack-5.6.21-2/apache2/htdocs/myorchid/application/views/templates/header.php
+INFO - 2016-05-13 01:57:42 --> Config Class Initialized
+INFO - 2016-05-13 01:57:42 --> Hooks Class Initialized
+DEBUG - 2016-05-13 01:57:42 --> UTF-8 Support Enabled
+INFO - 2016-05-13 01:57:42 --> Utf8 Class Initialized
+INFO - 2016-05-13 01:57:42 --> URI Class Initialized
+INFO - 2016-05-13 01:57:42 --> Router Class Initialized
+INFO - 2016-05-13 01:57:42 --> Output Class Initialized
+INFO - 2016-05-13 01:57:42 --> Security Class Initialized
+DEBUG - 2016-05-13 01:57:42 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-05-13 01:57:42 --> Input Class Initialized
+INFO - 2016-05-13 01:57:42 --> Language Class Initialized
+INFO - 2016-05-13 01:57:42 --> Loader Class Initialized
+INFO - 2016-05-13 01:57:42 --> Controller Class Initialized
+INFO - 2016-05-13 01:57:42 --> File loaded: /Applications/mampstack-5.6.21-2/apache2/htdocs/myorchid/application/views/templates/header.php
+INFO - 2016-05-13 01:57:56 --> Config Class Initialized
+INFO - 2016-05-13 01:57:56 --> Hooks Class Initialized
+DEBUG - 2016-05-13 01:57:56 --> UTF-8 Support Enabled
+INFO - 2016-05-13 01:57:56 --> Utf8 Class Initialized
+INFO - 2016-05-13 01:57:56 --> URI Class Initialized
+INFO - 2016-05-13 01:57:56 --> Router Class Initialized
+INFO - 2016-05-13 01:57:56 --> Output Class Initialized
+INFO - 2016-05-13 01:57:56 --> Security Class Initialized
+DEBUG - 2016-05-13 01:57:56 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-05-13 01:57:56 --> Input Class Initialized
+INFO - 2016-05-13 01:57:56 --> Language Class Initialized
+ERROR - 2016-05-13 01:57:56 --> 404 Page Not Found: %E3%85%81%E3%85%81/index
+INFO - 2016-05-13 01:57:59 --> Config Class Initialized
+INFO - 2016-05-13 01:57:59 --> Hooks Class Initialized
+DEBUG - 2016-05-13 01:57:59 --> UTF-8 Support Enabled
+INFO - 2016-05-13 01:57:59 --> Utf8 Class Initialized
+INFO - 2016-05-13 01:57:59 --> URI Class Initialized
+INFO - 2016-05-13 01:57:59 --> Router Class Initialized
+INFO - 2016-05-13 01:57:59 --> Output Class Initialized
+INFO - 2016-05-13 01:57:59 --> Security Class Initialized
+DEBUG - 2016-05-13 01:57:59 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-05-13 01:57:59 --> Input Class Initialized
+INFO - 2016-05-13 01:57:59 --> Language Class Initialized
+ERROR - 2016-05-13 01:57:59 --> 404 Page Not Found: Ee/index
+INFO - 2016-05-13 01:58:21 --> Config Class Initialized
+INFO - 2016-05-13 01:58:21 --> Hooks Class Initialized
+DEBUG - 2016-05-13 01:58:21 --> UTF-8 Support Enabled
+INFO - 2016-05-13 01:58:21 --> Utf8 Class Initialized
+INFO - 2016-05-13 01:58:21 --> URI Class Initialized
+DEBUG - 2016-05-13 01:58:21 --> No URI present. Default controller set.
+INFO - 2016-05-13 01:58:21 --> Router Class Initialized
+INFO - 2016-05-13 01:58:21 --> Output Class Initialized
+INFO - 2016-05-13 01:58:21 --> Security Class Initialized
+DEBUG - 2016-05-13 01:58:21 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-05-13 01:58:21 --> Input Class Initialized
+INFO - 2016-05-13 01:58:21 --> Language Class Initialized
+INFO - 2016-05-13 01:58:21 --> Loader Class Initialized
+INFO - 2016-05-13 01:58:21 --> Controller Class Initialized
+INFO - 2016-05-13 01:58:21 --> File loaded: /Applications/mampstack-5.6.21-2/apache2/htdocs/myorchid/application/views/welcome_message.php
+INFO - 2016-05-13 01:58:21 --> Final output sent to browser
+DEBUG - 2016-05-13 01:58:21 --> Total execution time: 0.0307
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
@@ -83,6 +84,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+
 </div>
 
 </body>
