de81a04ac8a1917d170656a073b0bbaf16b11632
Seobs <subi88@naver.com>
2016-05-13 17:14:05 +0900

    delete test2

---
+++ application/logs/log-2016-05-13.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2016-05-13 01:12:50 --> Config Class Initialized
+INFO - 2016-05-13 01:12:50 --> Hooks Class Initialized
+DEBUG - 2016-05-13 01:12:50 --> UTF-8 Support Enabled
+INFO - 2016-05-13 01:12:50 --> Utf8 Class Initialized
+INFO - 2016-05-13 01:12:50 --> URI Class Initialized
+DEBUG - 2016-05-13 01:12:50 --> No URI present. Default controller set.
+INFO - 2016-05-13 01:12:50 --> Router Class Initialized
+INFO - 2016-05-13 01:12:50 --> Output Class Initialized
+INFO - 2016-05-13 01:12:50 --> Security Class Initialized
+DEBUG - 2016-05-13 01:12:50 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-05-13 01:12:50 --> Input Class Initialized
+INFO - 2016-05-13 01:12:50 --> Language Class Initialized
+INFO - 2016-05-13 01:12:51 --> Loader Class Initialized
+INFO - 2016-05-13 01:12:51 --> Controller Class Initialized
+INFO - 2016-05-13 01:12:51 --> File loaded: /Applications/mampstack-5.6.21-2/apache2/htdocs/myorchid/application/views/templates/header.php
